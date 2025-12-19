6759ed37e7826d87e958e8f0c60953b6d267d819
Ubuntu <ubuntu@ip-172-31-19-243.ec2.internal>
2025-12-19 18:58:51 +0000

    files added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 function App() {
   const [items, setItems] = useState([]);
   const [name, setName] = useState('');
-  const API_BASE_URL = 'http://18.212.54.29:5000'
+  const API_BASE_URL = 'http://13.218.40.103:5000'
 
   const fetchItems = async () => {
     const res = await axios.get(`${API_BASE_URL}/api/items`);
